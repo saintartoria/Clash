87cf71dc427c94dd4892b0ad6d1b28e181ec2b99
saintartoria <63177702+saintartoria@users.noreply.github.com>
2021-02-26 22:44:48 +0800

    Create emby.js

---
+++ Ruleset/emby.js
@@ -0,0 +1,38 @@
+# > 流媒体emby-paoche服务器
+DOMAIN,emby.cjpc.xyz
+DOMAIN,emby.paoche.ooo
+# > 流媒体emby-flowercloud服务器
+DOMAIN,emby.flowercloud.yt
+# > 流媒体emby-maying服务器
+DOMAIN,emby.maying.club
+# > 流媒体emby-mdss服务器
+DOMAIN,emby.mdss.cloud
+# > 流媒体emby-nexitally服务器
+DOMAIN,emby.nexitally.com
+# > 流媒体emby-ytoo服务器
+DOMAIN,emby.ytoo.li
+# > 流媒体emby-prpr服务器
+DOMAIN,emby.prprcloud.com
+# > 流媒体emby-nyancat服务器
+DOMAIN,emby.nyancat.net
+# > 流媒体emby体验服
+DOMAIN,movie.xeton.dev
+# > 流媒体emby-Plus体验服
+DOMAIN,plus.xeton.dev
+DOMAIN,emby.xeton.dev
+DOMAIN,kodi.xeton.dev
+DOMAIN,jellyfin.xeton.dev
+DOMAIN,emby-best.xeton.dev
+# > 流媒体emby-Otaku体验服
+DOMAIN,emby.ok.edu.hk
+DOMAIN,emby.neko.edu.rs 
+DOMAIN,pub1.emby.wtf
+DOMAIN,pub1cf.emby.wtf
+# > 流媒体emby-卷毛鼠体验服
+DOMAIN,agahk1.jmsooo.com
+DOMAIN,agasg1.jmsooo.com
+DOMAIN,agajp1.jmsooo.com
+DOMAIN,aws.jmsooo.com
+DOMAIN,ntt1.jmsooo.com
+DOMAIN,hinet.jmsooo.com
+CF通用 : https://cf1.jmsooo.com:443
